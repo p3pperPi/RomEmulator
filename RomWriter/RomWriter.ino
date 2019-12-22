18df53b3a67b72b0ad61575ea7bbd2eb0ca7d1ea
p3pperPi <AgoDrill.habaneago@Gmail.com>
2019-12-22 18:38:36 +0900

    fix risky iterator

--- RomWriter/RomWriter.ino
+++ RomWriter/RomWriter.ino
@@ -4,33 +4,35 @@ byte data[] = {
 };
 
 
-void setup() {	
+void setup() {
 	for(int i = 0;i < sizeof(data);i++){
-		EEPROM.update(i,data[i]);
+		if(i < EEPROM.length()){
+			EEPROM.update(i,data[i]);
+		}
 	}
 	Serial.begin(38400);
 
 	delay(5000);
 
 	Serial.println();
-	
+
 	Serial.print("EEPROM Size : ");
 	Serial.print(EEPROM.length());
 	Serial.println("byte");
-	
+
 	Serial.print(sizeof(data));
 	Serial.println("byte written.");
 
 	for(int page = 0;page < 4;page++){
 		for(int upper = 0;upper <= 0x0F;upper++){
-			
+
 			Serial.print("p");
 			Serial.print(page);
 			Serial.print("/");
 			Serial.print(upper,HEX);
 			Serial.print("0 : ");
 			for(byte lower = 0;lower <= 0x0F;lower++){
-				int itr = 
+				int itr =
 					((page  & 0x03) << 8) |
 					((upper & 0x0F) << 4) |
 					((lower & 0x0F) << 0) ;
